8aaae4e4525b46213a316c1df8b1bdd6b41c8ebd
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-08-24 00:21:06 -0700

    Update bash script

--- apply.sh
+++ apply.sh
@@ -1,6 +1,11 @@
 #!/bin/env bash
 
-CONF_DIR="./config/conf"
+# Go into src directory
+cd src
+
+CONF_DIR="../config/conf"
+ICONS_DIR_LOCAL="../icons"
+ICONS_DIR="/boot/efi/EFI/refind/icons-custom"
 
 # If conf dir doesn't exists, create it
 if [ ! -d $CONF_DIR ]; then
@@ -8,15 +13,12 @@ if [ ! -d $CONF_DIR ]; then
 fi
 
 # Copy over icons from here to refind directory if not found
-if [ ! -d "/boot/efi/EFI/refind/icons-custom" ]; then
-    sudo cp -r ./icons /boot/efi/EFI/refind/icons-custom
+if [ ! -d $ICONS_DIR ]; then
+    sudo cp -r $ICONS_DIR_LOCAL $ICONS_DIR
 fi
 
 # Grab the constants inside python file
-eval "$(python3 src/constants.py)"
-
-# Go into src directory
-cd src
+eval "$(python3 constants.py)"
 
 # Create the .conf files
 python3 generate.py
@@ -25,7 +27,7 @@ python3 generate.py
 if [ -f $THEME_CONF ]; then
     cp $THEME_CONF $THEME_CONF.copy
     sudo chown root:root $THEME_CONF
-    sudo mv $THEME_CONF /boot/efi/EFI/refind
+    sudo mv $THEME_CONF /boot/efi/EFI/refind/
     echo "Copied theme.conf to root!"
 fi
 
@@ -33,6 +35,6 @@ fi
 if [ -f $REFIND_CONF ]; then
     cp $REFIND_CONF $REFIND_CONF.copy
     sudo chown root:root $REFIND_CONF
-    sudo mv $REFIND_CONF /boot/efi/EFI/refind
+    sudo mv $REFIND_CONF /boot/efi/EFI/refind/
     echo "Copied refind.conf to root!"
 fi
